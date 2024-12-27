fee5f4df23e77c58d9ddab2b5b476be58378331c
Aminul Hasan <bl8141@gmail.com>
2024-12-27 19:14:25 +0600

    fixed nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,72 +4,71 @@ import { signOut } from "firebase/auth";
 import { useAuth } from "../contexts/Auth";
 
 const Nav = () => {
-	const { userLoggedIn, role } = useAuth();
+  const { userLoggedIn, role } = useAuth();
 
-	return (
-		<header className="header">
-			<div className="container">
-				<nav className="header__navbar">
-					<ul>
-						<li>
-							<Link to="/">Home</Link>
-						</li>
+  return (
+    <header className="header">
+      <div className="container">
+        <nav className="header__navbar">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            {!userLoggedIn && (
+              <li>
+                <Link to="/sign-up">Sign Up</Link>
+              </li>
+            )}
 
-						<li>
-							<Link to="/sign-up">Sign Up</Link>
-						</li>
+            {/* <li>
+              <Link to="/admin/users">Users</Link>
+            </li> */}
 
-						<li>
-							<Link to="/admin/users">Users</Link>
-						</li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
 
-						<li>
-							<Link to="/cart">Cart</Link>
-						</li>
+            {userLoggedIn && (role === "super-admin" || role === "admin") && (
+              <li>
+                <Link to="/add-product">Add Product</Link>
+              </li>
+            )}
+            {userLoggedIn && (role === "super-admin" || role === "admin") && (
+              <li>
+                <Link to="/all-products">Products</Link>
+              </li>
+            )}
 
-						{userLoggedIn &&
-							(role === "super-admin" || role === "admin") && (
-								<li>
-									<Link to="/add-product">Add Product</Link>
-								</li>
-							)}
-						{userLoggedIn &&
-							(role === "super-admin" || role === "admin") && (
-								<li>
-									<Link to="/all-products">Products</Link>
-								</li>
-							)}
+            {/* <li>
+              <Link to="/my-orders">My Orders</Link>
+            </li> */}
 
-						<li>
-							<Link to="/my-orders">My Orders</Link>
-						</li>
+            {!userLoggedIn && (
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            )}
 
-						{!userLoggedIn && (
-							<li>
-								<Link to="/login">Login</Link>
-							</li>
-						)}
+            {/* <li>
+              <Link to="/admin/order-list">Order List</Link>
+            </li> */}
 
-						<li>
-							<Link to="/admin/order-list">Order List</Link>
-						</li>
-
-						{userLoggedIn && (
-							<li>
-								<button
-									onClick={() => {
-										signOut(auth);
-									}}
-								>
-									Logout
-								</button>
-							</li>
-						)}
-					</ul>
-				</nav>
-			</div>
-		</header>
-	);
+            {userLoggedIn && (
+              <li>
+                <button
+                  onClick={() => {
+                    signOut(auth);
+                  }}
+                >
+                  Logout
+                </button>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Nav;
